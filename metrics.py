98fc1f0fafaaee9b791fe56d2bb432f4c4283cb6
minus31 <n31fromsouth@seerslab.com>
2020-01-30 11:41:49 +0900

    fix

--- metrics.py
+++ metrics.py
@@ -6,16 +6,16 @@ def focal_loss(alpha=0.25, gamma=2):
         weight_a = alpha * (1 - y_pred) ** gamma * targets
         weight_b = (1 - alpha) * y_pred ** gamma * (1 - targets)
     
-        return (tf.log1p(tf.exp(-tf.abs(logits))) + tf.nn.relu(-logits)) * (weight_a + weight_b) + logits * weight_b 
+        return (tf.math.log1p(tf.exp(-tf.abs(logits))) + tf.nn.relu(-logits)) * (weight_a + weight_b) + logits * weight_b 
 
     def loss(y_true, y_pred):
         y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(), 1 - tf.keras.backend.epsilon())
-        logits = tf.log(y_pred / (1 - y_pred))
+        logits = tf.math.log(y_pred / (1 - y_pred))
 
-        loss = focal_loss_with_logits(logits=logits, targets=y_true, alpha=alpha, gamma=gamma, y_pred=y_pred)
+        loss_ = focal_loss_with_logits(logits=logits, targets=y_true, alpha=alpha, gamma=gamma, y_pred=y_pred)
 
         # or reduce_sum and/or axis=-1
-        return tf.reduce_mean(loss)
+        return tf.reduce_mean(loss_)
 
     return loss
 
