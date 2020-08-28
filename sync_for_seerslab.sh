543d601650d41391aaacec80e52360d435281a21
minus31 <donotgetgreed@gmail.com>
2020-08-28 14:14:34 +0900

    Fix name

--- sync_for_seerslab.sh
+++ sync_for_seerslab.sh
@@ -1,10 +1,15 @@
 files_str=$(git diff --name-only HEAD HEAD~1)
 commit_message=$(git show -s --format=%B HEAD)
 
+echo $files_str
+
 files=$(echo $files_str | tr " " "\n")
+
+echo Commit message : $commit_message 
 
 for file in $files
 do
+    echo $file
     cp -r $file ../seerslab_bitbucket/seersegmentation/
 done
 
